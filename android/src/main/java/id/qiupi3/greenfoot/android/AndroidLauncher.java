27d5c1cc2184f9ccfeb8fa300dcb08a596f75e18
Qiupi3 <trimas.putra7@gmail.com>
2025-09-23 13:22:53 +0700

    Initial Commit

---
+++ android/src/main/java/id/qiupi3/greenfoot/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package id.qiupi3.greenfoot.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import id.qiupi3.greenfoot.GreenfootGame;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new GreenfootGame(), configuration);
+    }
+}
---
+++ html/src/main/java/id/qiupi3/greenfoot/gwt/GwtLauncher.java
@@ -0,0 +1,26 @@
+package id.qiupi3.greenfoot.gwt;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import id.qiupi3.greenfoot.GreenfootGame;
+
+/** Launches the GWT application. */
+public class GwtLauncher extends GwtApplication {
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+            // Resizable application, uses available space in browser with no padding:
+            GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(true);
+            cfg.padVertical = 0;
+            cfg.padHorizontal = 0;
+            return cfg;
+            // If you want a fixed size application, comment out the above resizable section,
+            // and uncomment below:
+            //return new GwtApplicationConfiguration(640, 480);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+            return new GreenfootGame();
+        }
+}
